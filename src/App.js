ca6f1795e37bef760f3b9b11f18fc2e9a4a9be69
Gemma-MV <gemmavalenciano83@gmail.com>
2025-03-14 16:34:58 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>REACT</h1>
+      <h2>JSX</h2>
+      <h3>VIRTUAL DOM</h3>
+      <h4>HOLA MUNDO</h4>
+    </div>
+  );
+}
+
+export default App;
